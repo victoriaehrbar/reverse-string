c50665b4466cb4751fe68d4ad4eb8a21ae765ef4
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-16 15:54:09 -0500

    commit tree 64ff92c3de2a13390498ce5488afd30a4d47c264

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,11 @@
 function reverseString(str) {
-  // type your code here
+  let reversed = "";
+
+  for (let i = str.length - 1; i > -1; --i) {
+    reversed = reversed + str[i];
+  }
+
+  return reversed;
 }
 
 if (require.main === module) {
